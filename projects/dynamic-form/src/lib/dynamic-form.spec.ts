6313be1c1110c9d050272fed28f56b0c4b1debcf
Matthew Davis <matthew@matthewdavis.io>
2020-02-26 14:57:10 -0500

    bump

--- projects/dynamic-form/src/lib/dynamic-form.spec.ts
+++
@@ -1,7 +0,0 @@
-import { DynamicForm } from './dynamic-form';
-
-describe('DynamicForm', () => {
-  it('should create an instance', () => {
-    expect(new DynamicForm()).toBeTruthy();
-  });
-});
--- projects/dynamic-form/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of dynamic-form
- */
-
-export * from './lib/dynamic-form.service';
-export * from './lib/dynamic-form.component';
-export * from './lib/dynamic-form.module';
