960adffb19c6b3ab8cb75a3ad80e79f70a79d971
Matthew Davis <matthew@matthewdavis.io>
2020-02-26 14:55:51 -0500

    bump

--- projects/features-table/src/lib/features-table.spec.ts
+++
@@ -1,7 +0,0 @@
-import { FeaturesTable } from './features-table';
-
-describe('FeaturesTable', () => {
-  it('should create an instance', () => {
-    expect(new FeaturesTable()).toBeTruthy();
-  });
-});
--- projects/features-table/src/lib/features-table.ts
+++
@@ -1,7 +0,0 @@
-import { DatatableColumn } from '@angular.studio/datatable/datatable-column';
-
-export class FeaturesTable {
-
-    public columns: Array<DatatableColumn>;
-    public rows: Array<any>;
-}
