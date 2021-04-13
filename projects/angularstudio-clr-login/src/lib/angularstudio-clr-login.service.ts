0d42955607e50b2e17ab4559150e9850136ff28f
Matthew Davis <matthew@matthewdavis.io>
2021-04-13 18:09:51 -0400

    bump

--- projects/angularstudio-clr-login/src/lib/angularstudio-clr-login.service.ts
+++ projects/angularstudio-clr-login/src/lib/angularstudio-clr-login.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularStudioClrLoginConfig } from './angularstudio-clr-login-config';
-import { Subject, Observable } from 'rxjs';
+import { Subject, Observable, ReplaySubject } from 'rxjs';
 import { AngularStudioClrLoginEvent } from './angularstudio-clr-login-event';
 
 @Injectable({
@@ -10,7 +10,7 @@ export class AngularStudioClrLoginService {
 
     public config: AngularStudioClrLoginConfig;
     public events$: Subject<AngularStudioClrLoginEvent> = new Subject();
-    public defaults$: Subject<{ email: string }> = new Subject();
+    public defaults$: ReplaySubject<{ email: string }> = new ReplaySubject(1);
 
     public init(config: AngularStudioClrLoginConfig): Observable<AngularStudioClrLoginEvent> {
 
