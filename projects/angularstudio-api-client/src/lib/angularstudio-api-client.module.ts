75d1844485b8b8c2e0e072b77a77e2d877a470dd
Matthew Davis <matthew@matthewdavis.io>
2021-06-14 16:32:09 -0400

    bump

--- projects/angularstudio-api-client/src/lib/angularstudio-api-client.module.ts
+++ projects/angularstudio-api-client/src/lib/angularstudio-api-client.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { AngularStudioAPIClientConfig } from './angularstudio-api-client-config';
 import { AngularStudioAPIClientService } from './angularstudio-api-client-service';
+import { API_CLIENT_CONFIG } from "./angularstudio-api-client-config-token";
 
 @NgModule({
 
