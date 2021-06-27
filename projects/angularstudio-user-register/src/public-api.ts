8f8f4017a315eceae63dc378e5d57f89422f2010
Matthew Davis <matthew@matthewdavis.io>
2021-06-27 17:50:25 -0400

    bump

--- projects/angularstudio-user-register/src/public-api.ts
+++ projects/angularstudio-user-register/src/public-api.ts
@@ -1,7 +1,5 @@
-/*
- * Public API Surface of angularstudio-user-register
- */
-
 export * from './lib/angularstudio-user-register.service';
 export * from './lib/angularstudio-user-register.component';
 export * from './lib/angularstudio-user-register.module';
+export * from './lib/angularstudio-user-register-config';
+export * from './lib/angularstudio-user-register-result';
