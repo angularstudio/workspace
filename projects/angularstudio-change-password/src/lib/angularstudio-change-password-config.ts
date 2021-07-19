52dbeb0873ab059e30289e37c7d09d1fe1436c4c
Matthew Davis <matthew@matthewdavis.io>
2021-07-18 20:27:52 -0400

    bump

--- projects/angularstudio-change-password/src/lib/angularstudio-change-password-config.ts
+++ projects/angularstudio-change-password/src/lib/angularstudio-change-password-config.ts
@@ -1,5 +1,6 @@
 export class AngularStudioChangePasswordConfig {
 
+    public title?: string = 'Change Password';
     public message?: string;
 
     public constructor(config: AngularStudioChangePasswordConfig) {
